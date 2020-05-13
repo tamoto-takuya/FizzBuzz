4acc97a9a15c6588f495bd54589492448162091e
iwasaki <tamoto.takuya@m.alhinc.jp>
2020-05-13 15:35:20 +0900

    2th

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,5 +2,27 @@ package fizzbuzz;
 
 public class FizzBuzz {
 
+	public static String FizzBuzzProcessing(int number) {
+
+		if(number % 15 == 0) {
+			return "FizzBuzz";
+
+		} else if(number % 5 == 0) {
+			return "Buzz";
+
+		} else if(number % 3 == 0) {
+			return "Fizz";
+
+		} else {
+			return String.valueOf(number);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(FizzBuzzProcessing(i));
+		}
+	}
 
 }
