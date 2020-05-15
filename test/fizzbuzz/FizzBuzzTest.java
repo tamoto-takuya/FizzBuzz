96dc2fe50292bf679fa6369aa56ac945b47b758b
iwasaki <tamoto.takuya@m.alhinc.jp>
2020-05-15 15:01:59 +0900

    5th

--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -8,42 +8,42 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
 	@Test
-	public void testFizzBuzzProcessing_01() {
+	public void test引数15の場合FizzBuzz() {
 		assertEquals("FizzBuzz", FizzBuzz.FizzBuzzProcessing(15));
 	}
 
 	@Test
-	public void testFizzBuzzProcessing_02() {
+	public void test引数30の場合FizzBuzz() {
 		assertEquals("FizzBuzz", FizzBuzz.FizzBuzzProcessing(30));
 	}
 
 	@Test
-	public void testFizzProcessing_01() {
+	public void test引数3の場合Fizz() {
 		assertEquals("Fizz", FizzBuzz.FizzBuzzProcessing(3));
 	}
 
 	@Test
-	public void testFizzProcessing_02() {
+	public void test引数6の場合Fizz() {
 		assertEquals("Fizz", FizzBuzz.FizzBuzzProcessing(6));
 	}
 
 	@Test
-	public void testBuzzProcessing_01() {
+	public void test引数5の場合Buzz() {
 		assertEquals("Buzz", FizzBuzz.FizzBuzzProcessing(5));
 	}
 
 	@Test
-	public void testBuzzProcessing_02() {
+	public void test引数10の場合Buzz() {
 		assertEquals("Buzz", FizzBuzz.FizzBuzzProcessing(10));
 	}
 
 	@Test
-	public void testNumberProcessing_01() {
+	public void test引数4の場合4() {
 		assertEquals("4", FizzBuzz.FizzBuzzProcessing(4));
 	}
 
 	@Test
-	public void testNumberProcessing_02() {
+	public void test引数8の場合8() {
 		assertEquals("8", FizzBuzz.FizzBuzzProcessing(8));
 	}
 
